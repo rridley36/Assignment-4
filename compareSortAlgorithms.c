30b10fe8a285e109724ec401a3944e68c5746533
Russell Ridley <russ92003@gmail.com>
2023-03-15 16:34:15 -0400

    First Commit

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -7,16 +7,70 @@ int extraMemoryAllocated;
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
-void mergeSort(int pData[], int l, int r)
+
+void merge (int *pData, int l, int m, int r)
 {
-	
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Allocate space for holding array
+	int leftSide[n1];
+	int rightSide[n2];
+	extraMemoryAllocated = sizeof(leftSide) + sizeof(rightSide);
+
+	for (i = 0; i < n1; i++){ // Loads data from larger array to smaller arrays
+		leftSide[i] = pData[l+i];
+	}
+	for (j = 0; j < n2; j++){
+		rightSide[j] = pData[m + 1 + j];
+	}
+
+	i = 0; j = 0; k = l; // Resets variables
+
+	while (i < n1 && j < n2) // Sorts into merged array
+    {
+        if (leftSide[i] <= rightSide[j])
+        {
+            pData[k] = leftSide[i];
+            i++;
+        }
+        else
+        {
+        	pData[k] = rightSide[j];
+            j++;
+        }
+        k++;
+    }
+	while (i < n1)
+	{ // Transfers remaining elements
+		pData[k] = leftSide[i];
+		i++;
+		k++;
+	}
+	while (j < n2)
+	{ // Transfers remaining elements
+		pData[k] = rightSide[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int *pData, int l, int r)
+{
+	if (l < r){
+		int m = (l+r) / 2; // Calculates middle index;
+		mergeSort(pData, l, m);
+		mergeSort(pData, m+1, r);
+		merge(pData, l, m, r);
+	}
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	int i, item, j;
+	int i, item, j;	// No extra memory is allocated, such as memory for a holding array or holding variable
 	for (i = 1; i < n; i++) {
 		item = pData[i];
 			for(j=i-1; j>=0; j--) {
@@ -39,7 +93,7 @@ void bubbleSort(int* pData, int n)
 		for (int j = 0; j < n - i - 1; j++){
 			if (pData[j] > pData[j+1]) {
 				int temp = pData[j];
-				extraMemoryAllocated = sizeof(temp);
+				extraMemoryAllocated = sizeof(temp); // Bubble Sort only uses about 4 bytes of memory for a temporary holding variable
 				pData [j] = pData[j+1];
 				pData[j+1] = temp;
 			}
@@ -57,15 +111,15 @@ void selectionSort(int* pData, int n)
     {
         // Find the minimum element in unsorted array
         i_min = i;
-        for (j = i+1; j < n; j++){
-			if (pData[j] < pData[i_min]){
+        for (j = i+1; j < n; j++) {
+			if (pData[j] < pData[i_min]) {
 				i_min = j;
 		  	}
         // Swap the found minimum element with the first element
 		}
 		if (i_min != i){
 				temp = pData[i]; // Swap Data 
-				extraMemoryAllocated = sizeof(temp);
+				extraMemoryAllocated = sizeof(temp); // Selection Sort only uses about 4 bytes of memory for a temporary holding variable
 				pData[i] = pData[i_min];
 				pData[i_min] = temp;
 		   }
@@ -88,7 +142,7 @@ int parseData(char *inputFileName, int **ppData)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)malloc(sizeof(int) * dataSz);
-		if (ppData == NULL){
+		if (ppData == NULL) {
 			printf("Memory allocation failed.");
 		}
 		for (int i = 0; i < dataSz; i++){
@@ -171,7 +225,6 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		/*
 		
 		printf("Merge Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
@@ -183,7 +236,7 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		*/
+		
 		free(pDataCopy);
 		free(pDataSrc);
 	}
